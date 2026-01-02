55bba43a420ee06cb4973b3eb2bd487ac627ca7c
Mehul Arora <mehularora8385@gmail.com>
2026-01-02 10:15:56 -0500

    Add exam-wise dashboard with credential-based filtering (v3)

--- dist/dashboard-server.js
+++ dist/dashboard-server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Dashboard routes
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'exam-dashboard-v2.html'));
+    res.sendFile(path.join(__dirname, 'exam-dashboard-v3.html'));
 });
 
 app.get('/admin', (req, res) => {
